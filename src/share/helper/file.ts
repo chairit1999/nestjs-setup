2e4533428f1efb84e0b90f7cbb65c765a29f8a14
chairit1999 <chairit.2542@gmail.com>
2022-06-26 23:56:49 +0700

    update multer

--- src/share/helper/file.ts
+++ src/share/helper/file.ts
@@ -1,8 +1,5 @@
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
-
-export const BUCKET_WATER_METER = join(process.cwd(), 'bucket', 'water_meter');
-
 export const writeFileHelper = (
   path: string,
   file: Buffer,
